510b4f98ee1fa44ff9f18707f995fbe0f7c30ac4
Patryk <patryk.r.nowak@gmail.com>
2023-07-27 09:11:42 +0100

    router

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 // const express = require('express');
 // it is not experimental feature just typescript that is going to be compiled
 import express from "express"; // now you have it
+import todoRoutes from "./routes/todos";
 
 const app = express();
 
+app.use("/todos", todoRoutes);
+
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    res.status(500).json({ message: err.message });
+  }
+);
 
 app.listen(3000);
