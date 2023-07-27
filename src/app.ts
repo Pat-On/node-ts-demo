3880ef8d937fd6f733fe3c26567c325d2fa2779a
Patryk <patryk.r.nowak@gmail.com>
2023-07-27 08:51:09 +0100

    working - empty setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
-console.log("Something...");
+// const express = require('express');
+// it is not experimental feature just typescript that is going to be compiled
+import express from "express"; // now you have it
+
+const app = express();
+
+
+app.listen(3000);
